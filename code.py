df886cb675f6c36d2319b8929b7382f6c837f955
MennaOm <130208323+MennaOm@users.noreply.github.com>
2024-04-24 16:36:18 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,18 @@
-# import for using packedges
+
 import pandas as pd
 import nltk
 
-# read data
-df = pd.read_csv("paragraphs.txt",delimiter='\t')
 
-# to stop words 
+txt= pd.read_csv("paragraphs.txt",delimiter='\t')
+
+
 nltk.download('stopwords')
 from nltk.corpus import stopwords
-stw = set(stopwords.words('english'))
-#remove stop words
-df['text'] = df['text'].apply(lambda stwo : " ".join([w for w in str(stwo).split() if w not in stw]))
+stop_word = set(stopwords.words('english'))
 
-# to count each repated words after removing stopwords
+txt['text'] = txt['text'].apply(lambda stwo : " ".join([w for w in str(stwo).split() if w not in stop_word]))
+
+
 from collections import Counter
-words_count = df['text'].str.split(expand=True).stack().value_counts()
+words_count = txt['text'].str.split(expand=True).stack().value_counts()
 print(words_count)
